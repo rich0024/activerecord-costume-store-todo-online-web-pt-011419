44ee09d76159e33c0976a43afb617399c7ba1ece
Richard Yance <yance0022@gmail.com>
2019-05-07 17:04:09 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,8 +1,8 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_table :haunted_houses do |t|
-      t.string :name 
+      t.string :name
       t.string :location
       t.string :theme
       t.float :price
@@ -11,6 +11,6 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :closing_date
       t.text :description
       t.timestamps
-    end 
-  end 
+    end
+  end
 end
